f9173df62e9a625c1ca2bb2aea083da1fe39717c
sameer90417 <sameerkumar37555@gmail.com>
2024-03-20 00:37:23 +0530

    customizing session

---
+++ app/(guest_routes)/layout.tsx
@@ -0,0 +1,16 @@
+import { auth } from '@/auth'
+import { redirect } from 'next/navigation';
+import React, { ReactNode } from 'react'
+
+interface Props{
+    children : ReactNode
+}
+
+
+export default async function GuestLayout({ children } : Props) {
+  const session = await auth();
+  if(session) redirect('/');
+  return (
+    <div>{children}</div>
+  )
+}
